de9aa4e2d6697678e92f62f4706d3677cf1c6acb
Faraz <sayyedfaraz11@gmail.com>
2020-07-30 22:17:34 -0400

    Facebook SignIn

--- script3.js
+++ script3.js
@@ -2,30 +2,51 @@
     {
         username: "faraz",
         password: "supersecret"
+    },
+    {
+        username: "papa",
+        password: "123"
+    },
+    {
+        username: "mammi",
+        password: "786"
     }
 ];
 
 var newsFeed = [
     {
-          username: "wamiq",
+          username: "faraz",
           timeline: "So that from all that learning!"
     },
     {
         username: "papa",
         timeline: "JavaScript is soooo coool!"
-    } 
+    }, 
+    {
+        username: "mammi",
+        timeline: "JavaScript is soooo coool!"
+    }
 ];
 
 var userNamePrompt = prompt("What's your username");
 var passwordPrompt = prompt("What's your password?");
-
+ 
+function isUserValid(user, pass) {
+    for (var i=0; i < database.length; i++){
+        if (database[i].username === user &&
+            database[i].password === pass) {
+            return true;
+        } 
+    }
+    return false;
+}
+ 
 function signIn(user, pass) {
-    if (user === database[0].username &&
-         pass === database[0].password) {
-             console.log(newsFeed);
+    if (isUserValid(user, pass)) {
+            console.log(newsFeed); 
          } else {
              alert("Sorry, wrong username and password!");
          }
-}
+}  
 
 signIn(userNamePrompt, passwordPrompt);
