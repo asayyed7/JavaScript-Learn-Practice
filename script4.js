1bf98209b820ad74a8d11a00f91a8de2aa4e565b
Faraz <sayyedfaraz11@gmail.com>
2020-07-30 21:25:00 -0400

    Loops

---
+++ script4.js
@@ -0,0 +1,28 @@
+var todos = [
+    "clean room",
+    "brush teeth",
+    "exercise",
+    "study javascript!",
+    "eat healthy"
+];
+
+for (var i=0; i < todos.length; i++) {
+    console.log(todos[i], i);
+} 
+
+todos.forEach(function(todo, i) {
+    console.log(todo, i);
+}) 
+
+var counterOne = 0;
+while (counterOne < 10) {
+    console.log(counterOne);
+    counterOne++; 
+}
+
+var counterTwo = 10;
+do {
+    console.log(counterTwo);
+    counterTwo--;
+} while (counterTwo > 0); 
+
